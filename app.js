a73d6d411453feb694465903543835e01e504200
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-10-10 20:06:02 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,61 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    
+    const data = {
+        members: [
+            { 
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us2.api.mailchimp.com/3.0/lists/80cf993b82";
+
+    const options = {
+        method: "POST",
+        auth: "nigora1:19c33a2086104236fbdd2ddee5099319-us2"
+    }
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
+// {"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+// API key
+// 19c33a2086104236fbdd2ddee5099319-us2
+
+// List ID
+// 80cf993b82

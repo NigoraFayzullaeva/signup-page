0edcea774305de295506710d92ae4a41734a614f
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-10-10 21:56:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ app.post("/", function(req, res){
         auth: "nigora1:19c33a2086104236fbdd2ddee5099319-us2"
     }
     const request = https.request(url, options, function(response){
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -52,7 +59,11 @@ app.post("/", function(req, res){
 });
 // {"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect('/')
+})
+
+app.listen(process.env.PORT  || 3000, function(){
     console.log("Server is running on port 3000");
 });
 
